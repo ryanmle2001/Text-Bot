46f4533b0c8c5fb5d1b13bfe952addd21eacdc2e
Ryan Le <41928573+ryanmle2001@users.noreply.github.com>
2021-09-12 23:55:58 -0400

    Update twilio_credentials.py

--- twilio_credentials.py
+++ twilio_credentials.py
@@ -1,10 +1,6 @@
-# cellphone = "6176533683" # Felice
-# cellphone = "5088478062" # Ryan
-# cellphone = "5089632635" #cousin
 
-cellphone = "4704552723" # Emi
+cellphone = "" # Emi
 
-# cellphone = "01074353302" # Emi
 twilio_number = "15085523173" # Purchased phone number on Twilio
 twilio_account = ""
 twilio_token = ""
